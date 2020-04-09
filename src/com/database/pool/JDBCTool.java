4cbdcb6c3ebeb64d4d299d1dce57c1efc8651f5d
southwind1 <15936539335@163.com>
2020-04-09 15:09:39 +0800

    优化了数据库连接类

--- src/com/database/pool/JDBCTool.java
+++ src/com/database/pool/JDBCTool.java
@@ -4,6 +4,7 @@ import java.sql.Statement;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -19,6 +20,9 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 */
 public class JDBCTool {
 	private static DataSource dataSource;
+	protected static Statement s = null;
+	protected static ResultSet rs = null;
+	protected static Connection conn = null;
 	static {
 		try {
 			Properties p = new Properties();
@@ -51,13 +55,97 @@ public class JDBCTool {
 			if(dataSource.getConnection() == null) {
 				System.out.println("JDBCTool未获取到链接");
 			}
-			return dataSource.getConnection();
+			conn = dataSource.getConnection();
+			return conn;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException("获取链接失败，" + e.getMessage());
 		}
 	}
+	/**
+	 * 执行INSERT/UPDATE/DELETE SQL语句
+	 * @param sql SQL语句，字符串类型
+	 * @return 执行结果，int类型
+	 */
+	public static int executeUpdate(String sql)
+	{
+		int result = 0;
+		try {
+			s = getConn().createStatement();
+			result = s.executeUpdate(sql);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return result;
+	}
+	/**
+	 * 执行SELECT SQL语句
+	 * @param sql SQL语句，字符串类型
+	 * @return ResultSet结果集
+	 */
+	public static ResultSet executeQuery(String sql)
+	{
+		
+		try {
+			s = getConn().createStatement();
+			rs = s.executeQuery(sql);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	/**
+	 * 执行动态SQL语句
+	 * @param sql 含有参数的动态SQL语句。 
+	 * @return 返回PreparedStatement对象
+	 */
+	public static PreparedStatement executePreparedStatement(String sql)
+	{
+		PreparedStatement ps = null;
+		try
+		{
+			ps = getConn().prepareStatement(sql);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ps;
+	}
+	/**
+	 * 事务回滚
+	 */
+	public static void rollback() {
+		try {
+			getConn().rollback();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	/**
+	 * 关闭数据库连接对象
+	 */
+	public static void close()
+	{
+		try
+		{
+			if(rs!=null)
+				rs.close();
+			if(s!= null)
+				s.close();
+			if(conn!=null)
+				conn.close();
+			
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	public static void closeAll(Statement st,ResultSet rs,Connection conn) {
 		try {
 			if(st != null) {
